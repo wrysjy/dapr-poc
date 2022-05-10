af6949f7c0dde45d227f7777b0c313e137ac836a
Yu <warunyoo@opsta.in.th>
2022-05-10 17:57:11 +0700

    binding with cron

--- README.md
+++ README.md
@@ -21,3 +21,6 @@ docker-compose down
 ## Additional Resources:
 
 [Overview of Docker Compose](https://docs.docker.com/compose/)
+
+## Dapr Concepts
+[figma](https://www.figma.com/file/9JkczP2Qo5ReE8fgsuFfdi/Dapr-Concept?node-id=0%3A1)
--- node/app.js
+++ node/app.js
@@ -19,11 +19,11 @@ app.get('/order', (_req, res) => {
 
             return response.text();
         }).then((orders) => {
-            res.send(orders);
-        }).catch((error) => {
-            console.log(error);
-            res.status(500).send({message: error});
-        });
+        res.send(orders);
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).send({message: error});
+    });
 });
 
 app.post('/neworder', (req, res) => {
@@ -60,6 +60,39 @@ app.post('/dsstatus', (req, res) => {
     // const data = req.body.data;
     console.log("PubSub received a message!");
     res.sendStatus(200);
+});
+
+// binding cron
+const bindingName = "binding-cron";
+const bindingUrl = `http://localhost:${daprPort}/v1.0/bindings/${bindingName}`;
+let count = 0;
+app.post('/binding-cron', (req, res) => {
+    console.log("scheduled endpoint called ...")
+    res.sendStatus(200);
+    count++;
+    if (count === 3) {
+        console.log("scheduled endpoint called 3 times ...")
+        const data = {
+            'operation': 'delete'
+        }
+        fetch(bindingUrl, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((response) => {
+            if (!response.ok) {
+                throw "Failed to remove cron.";
+            }
+
+            console.log("Successfully remove cron.");
+            res.status(200).send();
+        }).catch((error) => {
+            console.log(error);
+            res.status(500).send({message: error});
+        });
+    }
 });
 
 app.listen(port, () => console.log(`Node App listening on port ${port}!`));
