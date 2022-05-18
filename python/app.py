4e9575bcecb454d89376682b073daa4d32fb13c3
Yu <warunyoo@opsta.in.th>
2022-05-18 15:45:10 +0700

    secret store

--- python/app.py
+++ python/app.py
@@ -13,6 +13,9 @@ dapr_url = "http://localhost:{}/v1.0/invoke/nodeapp/method/neworder".format(dapr
 # pub sub
 pub_sub_url = "http://localhost:{}/v1.0/publish/pubsub/deathStarStatus".format(dapr_port)
 
+# secret store
+secret_store_url = "http://localhost:{}/v1.0/secrets/localsecretstore/mysql".format(dapr_port)
+
 n = 0
 while True:
     n += 1
@@ -23,6 +26,8 @@ while True:
     try:
         requests.post(dapr_url, json=message)
         requests.post(pub_sub_url, json=message_pub_sub)
+        secret_store = requests.get(secret_store_url)
+        print("Secret store: {}".format(secret_store.text), flush=True)
     except Exception as e:
         print(e)
 
